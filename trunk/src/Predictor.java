c265c78c082db11c0c5be55809ed77582db5a276
hckirmizi <hckirmizi@e7ad5fdb-0fec-732c-5e43-db2142dee53a>
2010-04-22 23:20:34 +0000

    fixed styling.

--- trunk/src/Predictor.java
+++ trunk/src/Predictor.java
@@ -1,11 +1,11 @@
 /* =============================================================================
  * Name                   : Predictor.java
- * 							ProductionRule.java + Element.java
+ * 			    ProductionRule.java + Element.java
  * Author                 : Hakki Caner KIRMIZI
  * Description            : A java program which can evaluate an input grammar 
- * 							and computes the first and follow set of it. Then, 
- * 							it constructs a table of this result along with the 
- * 							prediction set for each specific production rule.
+ * 			    and computes the first and follow set of it. Then, 
+ * 			    it constructs a table of this result along with the 
+ * 			    prediction set for each specific production rule.
  * Environment            : Windows-7 Entreprise
  * Java IDE               : Eclipse Build id: 20100218-1602
  * Compiler               : javac 1.6.0_16
