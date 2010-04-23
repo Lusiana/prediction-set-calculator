eebe56b16f1f2208f930647195987b337e00a69e
hckirmizi <hckirmizi@e7ad5fdb-0fec-732c-5e43-db2142dee53a>
2010-04-23 18:39:22 +0000

    first, follow sets are ok. need to check for eof case on follow and lambda case on first set.

--- trunk/src/ProductionRule.java
+++ trunk/src/ProductionRule.java
@@ -2,42 +2,45 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class ProductionRule {
-	
+
 	private int id;
 	private Element LHS;
 	private ArrayList<Element> RHSList = new ArrayList<Element>();
-	private ArrayList<HashSet<Element>> FirstSet = 
-						new ArrayList<HashSet<Element>>();
-	private ArrayList<Element> FollowSet = new ArrayList<Element>();
+	private ArrayList<HashSet<Element>> FirstSet = new ArrayList<HashSet<Element>>();
+	private ArrayList<HashSet<Element>> FollowSet = new ArrayList<HashSet<Element>>();
 	private ArrayList<Element> PredictSet = new ArrayList<Element>();
 
 	ProductionRule(int id) {
 		this.id = id;
 	}
-	
+
 	public void initLHS(Element elem) {
 		this.LHS = elem;
 	}
-	
+
 	public void addToRHSList(Element elem) {
 		this.RHSList.add(elem);
 	}
-	
+
 	public ArrayList<Element> getRHSList() {
 		return this.RHSList;
 	}
-	
+
 	public void addToFirstSet(HashSet<Element> set) {
 		this.FirstSet.add(set);
 	}
-	
-	public void printRuleInfo(){
+
+	public void addToFollowSet(HashSet<Element> set) {
+		this.FollowSet.add(set);
+	}
+
+	public void printRuleInfo() {
 		System.out.print("ID: " + this.id + " --> ");
 		System.out.print("LHS: " + this.LHS + " --> ");
 		System.out.print("RHS: " + this.RHSList + " --> ");
-		System.out.println("FirstSet: " + this.FirstSet);
-		//System.out.println("FollowSet: " + this.FollowSet);
-		//System.out.println("PredictSet: " + this.PredictSet);
+		System.out.print("FirstSet: " + this.FirstSet + " --> ");
+		System.out.println("FollowSet: " + this.FollowSet);
+		// System.out.println("PredictSet: " + this.PredictSet);
 	}
-	
+
 }
